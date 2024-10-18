9edf57ccfdf666ea0c15f915549be3ee677a6aaf
petretiandrea <petretiandrea@gmail.com>
2024-10-18 08:43:41 +0200

    fix pre commit

--- src/rtp/99_locals.tf
+++ src/rtp/99_locals.tf
@@ -1,6 +1,6 @@
 locals {
-  project      = "${var.prefix}-${var.env_short}-${var.location_short}-${var.domain}"
-  product      = "${var.prefix}-${var.env_short}"
+  project = "${var.prefix}-${var.env_short}-${var.location_short}-${var.domain}"
+  product = "${var.prefix}-${var.env_short}"
 
   apim_name = "${local.product}-apim"
   apim_rg   = "${local.product}-api-rg"
