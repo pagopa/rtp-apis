a11e7dde9085920f02cce531756df2dd1a4e3947
Luca Consalvi <117908483+lucaconsalvi@users.noreply.github.com>
2025-01-20 09:35:20 +0100

    feat: [SRTP-210] change from segment to header versioning strategy api (#26)

--- src/rtp/02_api_service_provider.tf
+++ src/rtp/02_api_service_provider.tf
@@ -4,8 +4,9 @@ resource "azurerm_api_management_api_version_set" "rtp_service_provider_api" {
   api_management_name = data.azurerm_api_management.this.name
   resource_group_name = data.azurerm_api_management.this.resource_group_name
 
-  display_name      = "RTP Service Provider API"
-  versioning_scheme = "Segment"
+  display_name        = "RTP Service Provider API"
+  versioning_scheme   = "Header"
+  version_header_name = "Version"
 }
 
 resource "azurerm_api_management_api" "rtp_service_provider_api" {
