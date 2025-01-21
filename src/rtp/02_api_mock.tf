956ba7436a49971a3d5a1a39ec465ca8511c04c1
Andrea Morabito <andrea.morabito@pagopa.it>
2025-01-21 10:47:09 +0100

    add mock epc

--- src/rtp/02_api_mock.tf
+++ src/rtp/02_api_mock.tf
@@ -1,53 +1,37 @@
-## RTP Mock API ##
-resource "azurerm_api_management_api" "rtp_mock_api" {
-  name                = "${var.env_short}-rtp-mock-api"
+###### Mock EPC API ######
+resource "azurerm_api_management_api" "rtp_mock_api_epc" {
+  name                = "${var.env_short}-rtp-mock-api-epc"
   api_management_name = data.azurerm_api_management.this.name
   resource_group_name = data.azurerm_api_management.this.resource_group_name
 
   revision              = "1"
-  description           = "RTP MOCK API"
-  display_name          = "RTP MOCK API"
+  description           = "RTP MOCK API EPC"
+  display_name          = "RTP MOCK API EPC"
   path                  = "rtp/mock"
   protocols             = ["https"]
   subscription_required = false
 
   depends_on = [azurerm_api_management_product.rtp]
+
+  import {
+    content_format = "openapi"
+    content_value  = templatefile("./api/pagopa/EPC133-22_v3.1_SRTP_spec.openapi.yaml", {})
+  }
 }
 
-resource "azurerm_api_management_product_api" "rtp_mock_product_api" {
+resource "azurerm_api_management_product_api" "rtp_mock_product_api_epc" {
   api_management_name = data.azurerm_api_management.this.name
   resource_group_name = data.azurerm_api_management.this.resource_group_name
-  api_name            = azurerm_api_management_api.rtp_mock_api.name
+  api_name            = azurerm_api_management_api.rtp_mock_api_epc.name
   product_id          = azurerm_api_management_product.rtp.product_id
-  depends_on          = [azurerm_api_management_product.rtp, azurerm_api_management_api.rtp_mock_api]
+  depends_on          = [azurerm_api_management_product.rtp, azurerm_api_management_api.rtp_mock_api_epc]
 }
 
+resource "azurerm_api_management_api_operation_policy" "rtp_mock_send_srtp" {
+  api_name            = azurerm_api_management_api.rtp_mock_api_epc.name
+  api_management_name = azurerm_api_management_api.rtp_mock_api_epc.api_management_name
+  resource_group_name = azurerm_api_management_api.rtp_mock_api_epc.resource_group_name
+  operation_id        = "postRequestToPayRequests"
 
-## RTP Mock Operations ##
-resource "azurerm_api_management_api_operation" "rtp_mock_create_ticket" {
-  operation_id        = "rtp_mock_create_ticket"
-  api_name            = azurerm_api_management_api.rtp_mock_api.name
-  api_management_name = data.azurerm_api_management.this.name
-  resource_group_name = data.azurerm_api_management.this.resource_group_name
-  display_name        = "RTP Mock create ticket"
-  method              = "POST"
-  url_template        = "/api/v1/create"
-  description         = "Endpoint for create a rtp ticket api"
-}
-
-resource "azurerm_api_management_api_operation_policy" "rtp_mock_create_ticket_policy" {
-  api_name            = azurerm_api_management_api_operation.rtp_mock_create_ticket.api_name
-  api_management_name = azurerm_api_management_api_operation.rtp_mock_create_ticket.api_management_name
-  resource_group_name = azurerm_api_management_api_operation.rtp_mock_create_ticket.resource_group_name
-  operation_id        = azurerm_api_management_api_operation.rtp_mock_create_ticket.operation_id
-
-  xml_content = templatefile("./api/test/mock_policy.xml", {
-    env = var.env
-  })
-
-  depends_on = [
-    azurerm_api_management_api_operation.rtp_mock_create_ticket,
-    azurerm_api_management_policy_fragment.apim_rtp_validate_token
-  ]
-
+  xml_content = file("./api/test/mock_policy_epc.xml")
 }
