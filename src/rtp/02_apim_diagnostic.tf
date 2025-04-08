2e1dd82e268e9bbeb3ecab0cdfe6246e1e679122
TommasoLencioni <tommaso.lencioni@pagopa.it>
2025-04-08 16:41:10 +0200

    feat: [SRTP-408] add API diagnostics configuration for RTP APIs

---
+++ src/rtp/02_apim_diagnostic.tf
@@ -0,0 +1,29 @@
+locals {
+  rtp_apim_api_diagnostics = [
+    azurerm_api_management_api_version_set.rtp_activation_api.name,
+    azurerm_api_management_api_version_set.rtp_service_provider_api.name,
+    azurerm_api_management_api_version_set.rtp_callback_api.name,
+  ]
+}
+
+resource "azurerm_api_management_api_diagnostic" "rtp_apim_api_diagnostics" {
+  for_each = toset(local.rtp_apim_api_diagnostics)
+
+  identifier               = "applicationinsights"
+  resource_group_name      = data.azurerm_api_management.this.resource_group_name
+  api_management_name      = data.azurerm_api_management.this.name
+  api_name                 = each.key
+  api_management_logger_id = local.apim_logger_id
+
+  sampling_percentage       = 100.0
+  always_log_errors         = true
+  log_client_ip             = true
+  verbosity                 = "information"
+  http_correlation_protocol = "W3C"
+
+  frontend_request {
+    headers_to_log = [
+      "RequestId"
+    ]
+  }
+}
--- src/rtp/99_locals.tf
+++ src/rtp/99_locals.tf
@@ -2,8 +2,10 @@ locals {
   project = "${var.prefix}-${var.env_short}-${var.location_short}-${var.domain}"
   product = "${var.prefix}-${var.env_short}"
 
-  apim_name = "${local.product}-apim"
-  apim_rg   = "${local.product}-api-rg"
+  apim_name        = "${local.product}-apim"
+  apim_rg          = "${local.product}-api-rg"
+  apim_logger_name = "${local.product}-appinsights"
+  apim_logger_id   = "${data.azurerm_api_management.this.id}/loggers/${local.apim_logger_name}"
 
   rtp_base_url  = "https://api-rtp.${var.dns_zone_prefix}"
   rtp_fe_origin = "${var.domain}.${var.dns_zone_prefix}"
