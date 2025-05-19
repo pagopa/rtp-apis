fcf602820e7c572fb24b495622730160d917fb1a
Tommaso Lencioni <tommaso.lencioni@pagopa.it>
2025-05-19 16:14:06 +0200

    feat: [SRTP-430] Add payee registry API (#44)
    Co-authored-by: Michele Fucito <michele.fucito@digitouch.it>

--- src/rtp/01_apim_fragments.tf
+++ src/rtp/01_apim_fragments.tf
@@ -8,3 +8,16 @@ resource "azurerm_api_management_policy_fragment" "apim_rtp_validate_token" {
     mc_shared_base_url = "https://api-mcshared.${var.dns_zone_prefix}"
   })
 }
+
+
+
+resource "azurerm_api_management_policy_fragment" "apim_rtp_blob_storage_validate_token" {
+  name              = "rtp-validate-blob-storage-token-mcshared"
+  api_management_id = data.azurerm_api_management.this.id
+
+  description = "rtp-validate-blob-storage-token-mcshared"
+  format      = "xml"
+  value = templatefile("./api_fragment/validate-token-mcshared_blob_storage.xml", {
+    mc_shared_base_url = "https://api-mcshared.${var.dns_zone_prefix}"
+  })
+}
